7ee9ca351cfc0fa74bf884da85f13b676deae504
acidlady <nadja_reuther@freenet.de>
2025-04-19 13:42:02 +0200

    add: new Character with Style

---
+++ Suendenbock_App/Controllers/CharacterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Suendenbock_App.Controllers
+{
+    public class CharacterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateForm()
+        {
+            return View();
+        }
+    }
+}
---
+++ Suendenbock_App/Controllers/MagicClassController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Suendenbock_App.Controllers
+{
+    public class MagicClassController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
